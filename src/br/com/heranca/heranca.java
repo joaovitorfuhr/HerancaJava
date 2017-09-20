ee08c5f1ab9d93dfa336663c17dc4e1d859baf27
joaovitorfuhr <joao.fuhr@institutoivoti.org.br>
2017-09-19 21:36:31 -0300

    Primeiro teste

--- src/br/com/heranca/heranca.java
+++ src/br/com/heranca/heranca.java
@@ -2,4 +2,27 @@ package br.com.heranca;
 
 public class heranca {
 
+class Funcionario {
+	String nome;
+	String cpf;
+	double salario;
+}
+
+
+class Gerente {
+	String nome;
+	String cpf;
+	double salario;
+	int senha;
+	int numeroDeFuncionariosGerenciados;
+public boolean autentica(int senha) {
+	if (this.senha == senha) {
+	System.out.println("Acesso Permitido!");
+	return true;
+} else {
+	System.out.println("Acesso Negado!");
+	return false;
+	}
+}
+
 }
---
+++ src/br/com/heranca/teste/HerancaTeste.java
@@ -0,0 +1,13 @@
+package br.com.heranca.teste;
+
+public class HerancaTeste {
+
+	class TestaGerente {
+		public static void main(String[] args) {
+		Gerente gerente = new Gerente();
+		gerente.setNome("Jonas Gay");
+		gerente.setSenha(123);
+		}
+		}
+
+}
