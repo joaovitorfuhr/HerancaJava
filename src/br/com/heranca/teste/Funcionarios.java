9e63d1f86a9a9de0f803c45890daa81d21e552c6
joaovitorfuhr <joao.fuhr@institutoivoti.org.br>
2017-10-03 21:16:52 -0300

    Implementação dos testes dos funcionarios

---
+++ src/br/com/heranca/teste/Funcionarios.java
@@ -0,0 +1,32 @@
+package br.com.heranca;
+
+public class Funcionarios {
+
+	String nome;
+	String cpf;
+	double salario;
+
+	public String Getcpf() {
+		return this.cpf;
+	}
+
+	public double Getsalario(double salarioFuncio) {
+		return salario = salarioFuncio;
+	}
+
+	public String Cpf(String Cpf) {
+		return cpf = Cpf;
+	}
+
+	public String GetNome() {
+		return this.nome;
+	}
+
+	public double Salario(double salary) {
+		return salario = salary;
+	}
+
+	public String Nome(String name) {
+		return nome = name;
+	}
+}
